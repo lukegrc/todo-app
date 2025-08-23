0ec99c33c823d0244326a45cbc71d567fd4510bb
Luke Grech <58103361+lukegrc@users.noreply.github.com>
2025-08-23 17:04:46 +0200

    chore: upload files

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.4em;
+}
+</style>
